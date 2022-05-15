5f25c7b42c8215cf91677c54dbbb72d31384f382
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-05-15 21:20:51 +0800

    Add files via upload

---
+++ Example-Booking/Example-Booking/restaurantInfo.swift
@@ -0,0 +1,15 @@
+//
+//  restaurantInfo.swift
+//  Example-Booking
+//
+//  Created by 华嵩文 on 2022/5/14.
+//
+
+import SwiftUI
+
+var describe = "J'Go L'arome brings the latest Parisian trend, Bistronomie, to Sydney, with its Michelin quality and the relaxation of a corner bistro. The large, open kitchen is a popular feature, bringing the chef closer to the guests and making the preparation process more enjoyable and relaxing. With a few strokes of the brush, you can sense the French freedom and romantic elegance with just a glance. A graduate of the Paul Bocuse Institute, Lyon, France's top western culinary and hospitality school, Fang Fang's chef has worked in several Michelin-starred restaurants in France, and is particularly adept at presenting the art of French cuisine in a simple, natural yet refined way."
+
+var menu = ["Salade", "Ratatouille", "Cassoulet", "bourguignon", "Soufflé", "Flamiche", "Escargot", "Entrées", "Crêpe"]
+
+var time = ["11:00", "11:45",
+            "6:00", "6:45"]
